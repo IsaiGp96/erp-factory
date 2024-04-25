ae12606b5433bbb7304f31ca083199dabb902288
commodore_64 <cesarml2002@hotmail.com>
2024-04-25 01:07:13 -0600

    migration empleados

--- database/migrations/2024_04_25_065920_create_users_table.php
+++ database/migrations/2024_04_25_065920_create_users_table.php
@@ -24,7 +24,6 @@ return new class extends Migration
         });
 
 
-
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
             $table->string('token');

1c3468f747ca86ebd786332cfcfd84b52d79b64d
commodore_64 <cesarml2002@hotmail.com>
2024-04-25 00:53:40 -0600

    tabla users relacioanda con id_empleado

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,14 +12,18 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->unsignedBigInteger('id_empleado')->nullable();
             $table->timestamps();
+            $table->foreign('id_empleado')->references('id_empleado')->on('empleados');
         });
+
+
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
